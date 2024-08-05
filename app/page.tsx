6b152256471a508958f596208429f428c984cef5
Ndu ifeanyi <indu@fsdhgroup.com>
2024-08-05 09:07:07 +0100

    added search

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import CategoriesList from '@/components/home/CategoriesList';
+import PropertiesContainer from '@/components/home/PropertiesContainer';
 
-function page() {
+function HomePage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
+  // console.log(searchParams);
+
   return (
-    <div>page</div>
-  )
+    <section>
+      <CategoriesList
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+      <PropertiesContainer
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+    </section>
+  );
 }
-
-export default page
+export default HomePage;
