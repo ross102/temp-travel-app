4271ffb65ae7d90fd4972c3459287ea9b6b616a3
Ndu ifeanyi <indu@fsdhgroup.com>
2024-08-07 08:26:05 +0100

    feat: propertymap

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -11,7 +11,15 @@ import UserInfo from '@/components/properties/UserInfo';
 import { Separator } from '@/components/ui/separator';
 import Description from '@/components/properties/Description';
 import Amenities from '@/components/properties/Amenities';
-
+import dynamic from 'next/dynamic';
+import { Skeleton } from '@/components/ui/skeleton';
+const DynamicMap = dynamic(
+    () => import('@/components/properties/PropertyMap'),
+    {
+      ssr: false,
+      loading: () => <Skeleton className='h-[400px] w-full' />,
+    }
+  );
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
@@ -40,10 +48,11 @@ const profileImage = property.profile.profileImage;
         <PropertyRating inPage propertyId={property.id} />
       </div>
       <PropertyDetails details={details} />
-      <UserInfo profile={{ firstName, profileImage }} />;
+      <UserInfo profile={{ firstName, profileImage }} />
       <Separator className='mt-4' />
        <Description description={property.description} />
        <Amenities amenities={property.amenities} />
+       <DynamicMap countryCode={property.country} />
     </div>
     <div className='lg:col-span-4 flex flex-col items-center'>
       {/* calendar */}
