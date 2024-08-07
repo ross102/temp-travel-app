556f23f5d2fb66150bdcc4cbd6f8ca2f519ca3ef
Ndu ifeanyi <indu@fsdhgroup.com>
2024-08-07 09:41:07 +0100

    deploy

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import LinksDropDown from "./LinksDropDown";
 import DarkMode from "./DarkMode";
 import Logo from "./Logo";
 import { Suspense } from 'react';
-import LoadingCards from '@/app/loading';
+
 
 
 function Navbar() {
@@ -12,7 +12,7 @@ function Navbar() {
     <nav className='border-b'>
     <div className='container flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8'>
       <Logo />
-      <Suspense fallback={<LoadingCards />}>
+      <Suspense fallback={'...'}>
       <NavSearch />
       </Suspense>
       <div className='flex gap-4 items-center '>
